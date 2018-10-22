0456778038eed51659c46157770427f81922d415
Christian Leonhard Epping <christian.epping@mni.thm.de>
2018-10-22 21:00:05 +0200

    added zshprofile file path and stuff

--- .zshrc
+++ .zshrc
@@ -18,3 +18,4 @@ antigen init ~/.antigenrc
 
 #zsh shortcuts
 source ~/.aliases
+source ~/.zshprofile
--- install.sh
+++ install.sh
@@ -3,4 +3,9 @@
 cp .zshrc ~/.zshrc
 cp .aliases ~/.aliases
 cp .antigenrc ~/.antigenrc
+if [ ! -d "$(~/.antigen)" ] ; then
 git clone https://github.com/zsh-users/antigen.git ~/.antigen
+else
+cd ~/.antigen
+git pull
+fi
